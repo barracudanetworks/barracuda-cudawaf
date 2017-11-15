8e4a06f29e7f691896d68efdeb1f397809043166
bhanuapp <karanambhanu@gmail.com>
2017-11-15 11:58:46 +0530

    fixed puppet resource and updated logs to debug

--- lib/puppet/provider/wafservers/wafserverprovider.rb
+++ lib/puppet/provider/wafservers/wafserverprovider.rb
@@ -10,12 +10,11 @@ require 'rest-client'
 Puppet::Type.type(:wafservers).provide(:wafserverprovider) do
 
   Puppet.debug("Inside wafserverprovider: ")
-
   mk_resource_methods
 
-# this method will get server/servername and return true or false 
+# this method will get server/servername and return true or false
 def exists?
-  Puppet.info("Calling exists method of wafsererprovider: ")
+  Puppet.debug("Calling exists method of wafsererprovider: ")
   @property_hash[:ensure] == :present
 
   # getting waf authorization token
@@ -27,15 +26,16 @@ def exists?
 
 # call get service
 
-  serverName=@resource[:name]
-  serviceName=@resource[:service_name]
+  serverName = @resource[:name]
+  serviceName = @resource[:service_name]
   Puppet.debug("WAF server name in manifest:  #{serverName}")
+  Puppet.debug("WAF service name in manifest : #{serviceName}")
   # Check for the existance of the Service before calling the server api.
-  options={}
-  serviceresponse = service_instance.services_web_application_name_get(auth_header,serviceName,options)
+#  options={}
+  serviceresponse = service_instance.services_web_application_name_get(auth_header,serviceName,{})
   service_parsed_response = JSON.parse(serviceresponse)
   service_status_code=service_parsed_response["status_code"]
-  
+
   if serviceresponse.to_s.empty?
     fail("Not able to process the request. Please check the request parameters")
   end
@@ -67,56 +67,90 @@ end
 
 #this method get all servers from WAF system and builds the instances array
 def self.instances()
+ Puppet.debug("Calling getservices method")
+ services = getservices()
 
- Puppet.debug("Callling getInstances method of wafserverprovider: ")
-#  serviceName=@resource[:service_name]
-  serviceName="demo_service_13"
-  Puppet.debug("Service Name : #{serviceName}")
-  instances=[]
+ Puppet.debug("List of services .................. #{services}")
+ instances=[]
+ services.each do |service|
 
-  #FIXME FIXME FIXME FIXME
+   svc = service
+   Puppet.debug("Callling getInstances method of wafserverprovider: ")
+   serviceName=svc
+   Puppet.debug("Service Name : #{serviceName}")
+   login_instance = Login.new
+   auth_header = login_instance.get_auth_header
+   server_instance = SwaggerClient::ServerApi.new
+
+   # get all servers from WAF
+   data,status_code,headers = server_instance.services_web_application_name_servers_get(auth_header,serviceName,{})
+   Puppet.debug("WAF Get all servers response:    #{data}")
+
+   response = JSON.parse(data)
+   Puppet.debug("parsed response object is #{response}")
+   svrData =response["data"]
+   serviceName = response["Service"]
+   Puppet.debug("The DATA:::::: #{svrData}")
+   if svrData
+     svrData.each do |key,value|
+       servname = value["name"]
+       val = value
+       instances <<  new(
+         :ensure => :present,
+         :name => val["name"],
+         :service_name => response["Service"]
+       )
+     end
+   end
+
+  end # do end services
   return instances
+end
+
+
+#this method get all services from WAF system and builds the instances array
+def self.getservices
+
+  Puppet.debug("Callling getservices  method of wafserverprovider: ")
+  service_instances = []
 
   login_instance = Login.new
   auth_header = login_instance.get_auth_header
-  server_instance = SwaggerClient::ServerApi.new
+  service_instance = SwaggerClient::ServiceApi.new
 
-# get all servers from WAF
-  data,status_code,headers = server_instance.services_web_application_name_servers_get(auth_header,serviceName,{})
-  Puppet.debug("WAF Get all servers response:    #{data}")
-
-  response = JSON.parse(data)
-  Puppet.debug("parsed response object is #{response}")
-  svrData =response["data"]
-
-  Puppet.debug("The DATA:::::: #{svrData}")
-  if svrData
-    svrData.each do |key,value|
-      val = value
-      instances <<  new(
-       :ensure => :present,
-       :name => val["name"],
-       :service_name => val["service_name"]
-      )
-    end
- end
-
-  Puppet.debug("Instances .......... #{instances}")
-  return instances
+# get all services from WAF
+  data,status_code,headers = service_instance.services_get(auth_header,{})
+  Puppet.debug("WAF Get all services response:    #{data}")
+  unless data == '{}'
+    if status_code == 200
+      response = JSON.parse(data)
+      svcobj = response["object"]
+      Puppet.debug("Object is  #{svcobj}")
+      svcData = response["data"]
+      Puppet.debug("Service  data - having servers:  #{svcData}")
+      svcData.each do |key,value|
+        service_instances.push(value["name"])
+      end
+   end # if end
+ end  # unless end
+ return service_instances
 
 end
+
+
+
 
 # this method compares the name attribute from instances and resources. If it matches then sets the provider
 def self.prefetch(resources)
 
   Puppet.debug("Calling self.prefetch method of wafserverprovider: ")
   servers = instances
-  resources.keys.each do |name|
-     if provider = servers.find { |server| server.name == name}
+  resources.each do |name,service_name|
+     if provider = servers.find { |server| server.name == name && server.service_name == service_name}
+#     if provider = servers.find { |server| server.name == name}
      resources[name].provider=provider
-     end    
+     end
   end
-
  end
 
 # this method does a put call to waf servers. This will be triggered with ensure is present and exists method return true.
@@ -128,17 +162,17 @@ def flush
      server_instance = SwaggerClient::ServerApi.new
      serverName=@resource[:name]
      serviceName=@resource[:service_name]
-     Puppet.debug("WAF server name in manifest: #{serverName}")     
+     Puppet.debug("WAF server name in manifest: #{serverName}")
 
      data,status_code,headers = server_instance.services_web_application_name_servers_server_name_put(auth_header,serviceName,serverName,message(resource),{})
 
-     if status_code == 200 
+     if status_code == 200
         return data
      else
         Puppet.debug("There is some problem to process the request. status_code is #{status_code}")
         return
      end
-  end    
+  end
 end
 
 # this is a util method to build the JSON array to post the request to WAF
@@ -166,11 +200,11 @@ def message(object)
 
      if hostname.nil?
         fail("hostname is empty. Please add a valid hostname since the identifier is set to hostname.")
-     else 
+     else
        # opts["identifier"] = "Hostname"
        # opts["hostname"] = hostname
        parameters[:identifier]="Hostname"
-       parameters[:hostname]=hostname 
+       parameters[:hostname]=hostname
      end
   else
      ip_address = nil
@@ -182,7 +216,7 @@ def message(object)
         else
            #opts["identifier"] = "IP Address"
            #opts["ip-address"] = ip_address
-	    parameters[:identifier]="IP Address"
+            parameters[:identifier]="IP Address"
             parameters[:ip_address]=ip_address
         end
      else
@@ -228,7 +262,7 @@ def create
   return result
 end
 
-# this method will call the delete api of a WAF service 
+# this method will call the delete api of a WAF service
 def destroy
   Puppet.debug("Calling wafserverprovider destroy method: ")
   # getting the authorization token for WAF.
@@ -239,7 +273,7 @@ def destroy
   serverName=@resource[:name]
   serviceName=@resource[:service_name]
   Puppet.debug("WAF server name in manifest: #{serverName}")
-  response = server_instance.services_web_application_name_servers_server_name_delete(auth_header,serviceName,serverName,{})   
+  response = server_instance.services_web_application_name_servers_server_name_delete(auth_header,serviceName,serverName,{})
   # We clear the hash here to stop flush from triggering.
   @property_hash.clear
   return response
