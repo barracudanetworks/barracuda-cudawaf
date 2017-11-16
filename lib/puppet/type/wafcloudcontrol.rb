d8f01fe595742aa3eb42a92e4da7db285320548d
Ranjyoti Prakash <ranjyotiprakashsingh@gmail.com>
2017-11-16 13:05:51 +0000

    Fix Puppet types - Added proper validations for the types

--- lib/puppet/type/wafcloudcontrol.rb
+++ lib/puppet/type/wafcloudcontrol.rb
@@ -1,27 +1,27 @@
 Puppet::Type.newtype(:wafcloudcontrol) do
   @doc = 'Type representing a Cloud Control object on the Barracuda Web Application Firewall.'
 
-ensurable
+  ensurable
+
+
   newparam(:state, :namevar => true) do
-    desc ''
-    validate do |value|
-      fail 'state should not be blank' if value == ''
-    end
+    desc 'Cloud Control Connection State'
+    defaultto 'not-connected'
+    newvalues('connected', 'not-connected')
   end
 
   newproperty(:connect_mode) do
-    desc 'connect_mode'
-    defaultto:'cloud'
+    desc 'Connected Using'
+    defaultto :cloud
+    newvalues(:cloud, :local)
   end
 
   newproperty(:password) do
-   desc 'password'
-   defaultto:''
+   desc 'Password'
   end
  
   newproperty(:username) do
-   desc 'username'
-   defaultto:''
+   desc 'Username'
   end
 
   newproperty(:validation_token) do
