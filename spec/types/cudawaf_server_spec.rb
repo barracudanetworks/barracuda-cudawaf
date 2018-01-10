cdc8c014e56353569796028bec6db0e9dc401c0a
bkaranam <bkaranam@barracuda.com>
2018-01-10 12:25:41 +0000

    Adding test cases for types

---
+++ spec/types/cudawaf_server_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+# require 'puppetlabs_spec_helper/module_spec_helper'
+
+type_class = Puppet::Type.type(:cudawaf_server)
+
+describe type_class do
+  let :params do
+    %i[
+      name
+      service_name
+    ]
+  end
+
+  let :properties do
+    %i[
+      address_version
+      status
+      hostname
+      comments
+      port
+      identifier
+      ip_address
+    ]
+  end
+
+  it 'should have expected properties' do
+    properties.each do |property|
+      expect(type_class.properties.map(&:name)).to be_include(property)
+    end
+  end
+
+  it 'should have expected parameters' do
+    params.each do |param|
+      expect(type_class.parameters).to be_include(param)
+    end
+  end
+
+  it 'should require a name' do
+    expect do
+      type_class.new({})
+    end.to raise_error(Puppet::Error, 'Title or name must be provided')
+  end
+
+  it 'should support :present as a value to :ensure' do
+    type_class.new(name: 'waftestserver1', ensure: :present)
+  end
+end
