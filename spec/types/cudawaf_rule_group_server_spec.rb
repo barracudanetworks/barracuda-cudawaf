5a8f7af14399f1dd292df5403356734ec5373432
bkaranam <bkaranam@barracuda.com>
2018-01-10 13:25:05 +0000

    fixing rule_group_server type and spec file

---
+++ spec/types/cudawaf_rule_group_server_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+type_class = Puppet::Type.type(:cudawaf_rule_group_server)
+describe type_class do
+  let :params do
+    %i[
+      name
+      service_name
+      rule_group_name
+    ]
+  end
+
+  let :properties do
+    %i[
+      address_version
+      backup_server
+      comments
+      hostname
+      identifier
+      ip_address
+      port
+      status
+      weight
+    ]
+  end
+
+  it 'should have expected properties' do
+    properties.each do |property|
+      expect(type_class.properties.map(&:name)).to be_include(property)
+    end
+  end
+
+  it 'should have expected parameters' do
+    params.each do |param|
+      expect(type_class.parameters).to be_include(param)
+    end
+  end
+
+  it 'should require a name' do
+    expect do
+      type_class.new({})
+    end.to raise_error(Puppet::Error, 'Title or name must be provided')
+  end
+
+  it 'should support :present as a value to :ensure' do
+    type_class.new(name: 'httpsApp1', ensure: :present)
+  end
+end
