73df07fa7d5658995632253454b3e4f992e92445
Sivaraman Kannan <skannan@barracuda.com>
2018-01-12 10:21:18 +0000

    Addressing review comments.

--- lib/puppet/feature/cudawaf_device.rb
+++ lib/puppet/feature/cudawaf_device.rb
@@ -6,19 +6,19 @@ Puppet.features.add(:cudawaf_device) do
   begin
     transport = nil
     if Puppet::Util::NetworkDevice.current
-      #we are in `puppet device`
+      # we are in `puppet device`
       transport = Puppet::Util::NetworkDevice.current.transport
     else
-      #we are in `puppet resource`
+      # we are in `puppet resource`
       transport = Puppet::Util::NetworkDevice::Transport::Cudawaf.new(Facter.value(:url))
     end
-    facts     = Puppet::Util::NetworkDevice::Cudawaf::Facts.new(transport).retrieve
-    if facts and facts[:operatingsystem] == :Cudawaf
+    facts = Puppet::Util::NetworkDevice::Cudawaf::Facts.new(transport).retrieve
+    if facts && (facts[:operatingsystem] == :Cudawaf)
       true
     else
       false
     end
-  rescue
+  rescue StandardError
     false
   end
 end
